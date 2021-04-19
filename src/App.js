89281056047e8e39361942e2b96754fe85a73fa9
bitbeckers <code@bitbeckers.com>
2021-04-19 20:49:53 +0200

    updated domaintype and data. refactored to ethers. failing on signature-signer mismatch

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import {
   NotificationManager,
 } from "react-notifications";
 import "react-notifications/lib/notifications.css";
-import Web3 from "web3";
+
+import { ethers } from "ethers";
 import { Biconomy } from "@biconomy/mexa";
 import Torus from "@toruslabs/torus-embed";
+
 import { makeStyles } from "@material-ui/core/styles";
 import Link from "@material-ui/core/Link";
 import Typography from "@material-ui/core/Typography";
@@ -16,13 +18,11 @@ import { Box } from "@material-ui/core";
 
 import singlePlayerCommitAbi from "./spcAbi.json";
 
-const BN = Web3.utils.BN;
-
 let sigUtil = require("eth-sig-util");
 
 let config = {};
 config.contract = {
-  address: "0x6A383cf1F8897585718DCA629a8f1471339abFe4",
+  address: "0x6B6FD55b224b25B2F56A10Ce670B097e66Fca136",
   abi: singlePlayerCommitAbi,
   rpcUrl: "https://rpc-mumbai.maticvigil.com/",
 };
@@ -64,28 +64,29 @@ const metaTransactionType = [
   { name: "functionSignature", type: "bytes" },
 ];
 
-const domainData = {
-  name: "SinglePlayerCommit",
-  version: "1",
-  verifyingContract: config.contract.address,
-  salt: "0x" + (80001).toString(16).padStart(64, "0"),
-};
+// const domainData = {
+//   name: "SinglePlayerCommit",
+//   version: "1",
+//   verifyingContract: config.contract.address,
+//   salt: "0x" + (80001).toString(16).padStart(64, "0"),
+// };
 
 const commitment = {
-  activityKey:
+  _activityKey:
     "0x7c811a21b2f664032d44e98853a7045b8e6b13994831bf3b1d0a63ca27960b4c",
-  depositAmount: new BN(1).toString(),
-  endTime: 1617919200,
-  goalValue: 10,
-  stake: Web3.utils.toWei("1"),
-  startTime: 1617790842,
-  userId: "45977566",
+  _depositAmount: "1",
+  _endTime: 1617919200,
+  _goalValue: 100,
+  _stake: 1,
+  _startTime: 1617790842,
+  _userId: "45977566",
 };
 
-let web3;
-let provider;
 let torus;
-let contract;
+let salt = 42;
+let walletProvider, walletSigner;
+let contract, contractInterface;
+let biconomy;
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -98,12 +99,22 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const showErrorMessage = (message) => {
+  NotificationManager.error(message, "Error", 5000);
+};
+
+const showSuccessMessage = (message) => {
+  NotificationManager.success(message, "Message", 3000);
+};
+
+const showInfoMessage = (message) => {
+  NotificationManager.info(message, "Info", 3000);
+};
+
 function App() {
   const classes = useStyles();
   const preventDefault = (event) => event.preventDefault();
-  const [quote, setQuote] = useState("This is a default quote");
   const [owner, setOwner] = useState("Default Owner Address");
-  const [newQuote, setNewQuote] = useState("");
   const [selectedAddress, setSelectedAddress] = useState("");
   const [metaTxEnabled, setMetaTxEnabled] = useState(true);
   const [transactionHash, setTransactionHash] = useState("");
@@ -115,6 +126,7 @@ function App() {
         window.ethereum.isMetaMask
       ) {
         // Ethereum user detected. You can now use the provider.
+        //Torus is the provider used for signing the transaction
         torus = new Torus({
           buttonPosition: "bottom-left",
         });
@@ -123,38 +135,47 @@ function App() {
           buildEnv: "production",
           enableLogging: true,
           network: {
-            //host: "rinkeby",
+            host: "mumbai",
             chainId: 80001,
             networkName: "Mumbai Test Network",
           },
           showTorusButton: true,
         });
 
-        await torus.login().then(account => setSelectedAddress(account[0]));
-        console.log("Torus: ", torus)
+        await torus.login().then((account) => setSelectedAddress(account[0]));
+        console.log("Torus: ", torus);
 
-        provider = window.ethereum;
+        //This set of providers is userd for communicating with the network
+        let jsonRpcProvider = new ethers.providers.JsonRpcProvider(
+          "https://rpc-mumbai.matic.today"
+        );
 
-        const biconomy = new Biconomy(provider, {
-          apiKey: "gZT51Vc7u.69fff9c5-4afe-4961-aff1-41ab237f97f6",
+        biconomy = new Biconomy(jsonRpcProvider, {
+          walletProvider: torus.provider,
+          apiKey: "V7nbIe8Ue.94e3d8fd-2f0d-42cc-96aa-27d57dac9a7c",
           debug: true,
         });
 
-        // This web3 instance is used to read normally and write to contract via meta transactions.
-        web3 = new Web3(biconomy);
+        console.log("Biconomy: ", biconomy);
 
-        // web3 = new Web3(provider);
+        walletProvider = new ethers.providers.Web3Provider(torus.provider);
+        walletSigner = walletProvider.getSigner();
+        // This web3 instance is used to read normally and write to contract via meta transactions.
+        // web3 = new Web3(biconomy);
+
+        let userAddress = await walletSigner.getAddress();
+        setSelectedAddress(userAddress);
+
         biconomy
           .onEvent(biconomy.READY, () => {
             console.log("READY");
-            contract = new web3.eth.Contract(
+            contract = new ethers.Contract(
+              config.contract.address,
               config.contract.abi,
-              config.contract.address
+              biconomy.getSignerByAddress(userAddress)
             );
-            // setSelectedAddress(provider.selectedAddress);
-            // provider.on("accountsChanged", function (accounts) {
-            //   setSelectedAddress(accounts[0]);
-            // });
+
+            contractInterface = new ethers.utils.Interface(config.contract.abi);
           })
           .onEvent(biconomy.ERROR, (error, message) => {
             console.log("ERROR: ", error);
@@ -167,31 +188,29 @@ function App() {
     init();
   }, []);
 
-  const onQuoteChange = (event) => {
-    setNewQuote(event.target.value);
-  };
-
   const onSubmit = async (event) => {
     console.log(contract);
     if (contract) {
       setTransactionHash("");
       if (metaTxEnabled) {
         console.log("Sending meta transaction");
-        console.log("SelectedAddress: ", selectedAddress)
+        console.log("SelectedAddress: ", selectedAddress);
         let userAddress = selectedAddress;
-        let nonce = await web3.eth.getTransactionCount(userAddress, 'pending');
+        let nonce = await contract.getNonce(userAddress);
 
-        let functionSignature = contract.methods
-          .depositAndCommit(
-            commitment.activityKey,
-            commitment.goalValue,
-            commitment.startTime,
-            commitment.endTime,
-            commitment.stake,
-            commitment.stake,
-            commitment.userId
-          )
-          .encodeABI();
+        let functionSignature = contractInterface.encodeFunctionData(
+          "depositAndCommit",
+          [
+            commitment._activityKey,
+            commitment._goalValue,
+            commitment._startTime,
+            commitment._endTime,
+            commitment._stake,
+            commitment._stake,
+            commitment._userId,
+          ]
+        );
+
         let message = {};
         message.nonce = parseInt(nonce);
         message.from = userAddress;
@@ -202,13 +221,14 @@ function App() {
             EIP712Domain: domainType,
             MetaTransaction: metaTransactionType,
           },
-          domain: domainData,
+          domain: commitment,
           primaryType: "MetaTransaction",
           message: message,
         });
-        console.log(domainData);
+
+        console.log(commitment);
         console.log("Trying to sign and send");
-        await torus.provider.send(
+        await torus.provider.sendAsync(
           {
             jsonrpc: "2.0",
             id: 999999999999,
@@ -222,8 +242,8 @@ function App() {
             } else if (response && response.result) {
               let { r, s, v } = getSignatureParameters(response.result);
               console.log(userAddress);
-              console.log(JSON.stringify(message));
-              console.log(message);
+              console.log(JSON.stringify(dataToSign));
+              console.log(message)
               console.log(getSignatureParameters(response.result));
 
               const recovered = sigUtil.recoverTypedSignature_v4({
@@ -231,22 +251,12 @@ function App() {
                 sig: response.result,
               });
               console.log(`Recovered ${recovered}`);
-              sendTransaction(userAddress, functionSignature, r, s, v);
+              sendSignedTransaction(userAddress, functionSignature, r, s, v);
             }
           }
         );
       } else {
-        console.log("Sending normal transaction");
-        contract.methods
-          .setQuote(newQuote)
-          .send({ from: selectedAddress })
-          .on("transactionHash", function (hash) {
-            showInfoMessage(`Transaction sent to blockchain with hash ${hash}`);
-          })
-          .once("confirmation", function (confirmationNumber, receipt) {
-            setTransactionHash(receipt.transactionHash);
-            showSuccessMessage("Transaction confirmed");
-          });
+        showErrorMessage("MetaTx not enabled");
       }
     } else {
       showErrorMessage("Please enter the quote");
@@ -254,7 +264,7 @@ function App() {
   };
 
   const getSignatureParameters = (signature) => {
-    if (!web3.utils.isHexStrict(signature)) {
+    if (!ethers.utils.isHexString(signature)) {
       throw new Error(
         'Given value "'.concat(signature, '" is not a valid hex string.')
       );
@@ -262,7 +272,7 @@ function App() {
     var r = signature.slice(0, 66);
     var s = "0x".concat(signature.slice(66, 130));
     var v = "0x".concat(signature.slice(130, 132));
-    v = web3.utils.hexToNumber(v);
+    v = ethers.BigNumber.from(v).toNumber();
     if (![27, 28].includes(v)) v += 27;
     return {
       r: r,
@@ -271,70 +281,51 @@ function App() {
     };
   };
 
-  const showErrorMessage = (message) => {
-    NotificationManager.error(message, "Error", 5000);
-  };
+  const sendSignedTransaction = async (userAddress, functionData, r, s, v) => {
+    showInfoMessage("Sending signed transaction");
 
-  const showSuccessMessage = (message) => {
-    NotificationManager.success(message, "Message", 3000);
-  };
+    try {
+      // let gasLimit = await contract
+      //   .executeMetaTransaction(userAddress, functionData, r, s, v)
+      //   .estimateGas({ from: userAddress });
+      // let gasPrice = await ethersProvider.getGasPrice();
 
-  const showInfoMessage = (message) => {
-    NotificationManager.info(message, "Info", 3000);
-  };
+      let overrides = {
+        from: userAddress,
+        gasLimit: 350000,
+      };
 
-  const sendTransaction = async (userAddress, functionData, r, s, v) => {
-    if (web3 && contract) {
-      try {
-        let gasLimit = await contract.methods
-          .executeMetaTransaction(userAddress, functionData, r, s, v)
-          .estimateGas({ from: userAddress });
-        let gasPrice = await web3.eth.getGasPrice();
-        let tx = contract.methods
-          .executeMetaTransaction(userAddress, functionData, r, s, v)
-          .send({
-            from: userAddress,
-          });
+      // let gasLimit = await contract.estimateGas.executeMetaTransaction(
+      //   userAddress,
+      //   functionData,
+      //   r,
+      //   s,
+      //   v, overrides
+      // );
 
-        tx.on("transactionHash", function (hash) {
-          console.log(`Transaction hash is ${hash}`);
-          showInfoMessage(`Transaction sent by relayer with hash ${hash}`);
-        }).once("confirmation", function (confirmationNumber, receipt) {
-          console.log(receipt);
-          setTransactionHash(receipt.transactionHash);
-          showSuccessMessage("Transaction confirmed on chain");
-          // getQuoteFromNetwork();
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  };
-  const getTransactionReceiptMined = (txHash, interval) => {
-    const self = this;
-    const transactionReceiptAsync = async function (resolve, reject) {
-      var receipt = await web3.eth.getTransactionReceipt(txHash);
-      if (receipt == null) {
-        setTimeout(
-          () => transactionReceiptAsync(resolve, reject),
-          interval ? interval : 500
-        );
-      } else {
-        resolve(receipt);
-      }
-    };
+      let tx = await contract.executeMetaTransaction(
+        userAddress,
+        functionData,
+        r,
+        s,
+        v, overrides
+      );
 
-    if (typeof txHash === "string") {
-      return new Promise(transactionReceiptAsync);
-    } else {
-      throw new Error("Invalid Type: " + txHash);
+      showInfoMessage(`Transaction sent. Waiting for confirmation ..`);
+      await tx.wait(1);
+      console.log("Transaction hash : ", tx.hash);
+      console.log(tx);
+      setTransactionHash(tx.hash);
+
+      showSuccessMessage("Transaction confirmed on chain");
+    } catch (error) {
+      console.log(error);
     }
   };
 
   return (
     <div className="App">
       <section className="main">
-
         <div className="mb-attribution">
           <p className="mb-author">{owner}</p>
           <cite>Click to send preconfigured transaction</cite>
@@ -362,9 +353,6 @@ function App() {
             <Button variant="contained" color="primary" onClick={onSubmit}>
               Deposit DAI and create Commitment
             </Button>
-            {/* <Button variant="contained" color="primary" onClick={onSubmitWithPrivateKey} style={{marginLeft: "10px"}}>
-              Submit (using private key)
-            </Button> */}
           </div>
         </div>
       </section>
